cd6fc04d7f96e86db61711c34573cfb79f22932e
Marcus Lee <marcustutorial@hotmail.com>
2022-08-14 22:32:23 +0800

    added cli rust app

--- apps/app/src/server/trpc/router/finance.ts
+++ apps/app/src/server/trpc/router/finance.ts
@@ -1,31 +1,8 @@
 import { ExpenseFindManySchema } from '@/server/trpc/generated/schemas/findManyExpense.schema';
-import { authedProcedure, t } from '@/server/trpc/utils';
+import { t } from '@/server/trpc/utils';
 
 import { ExpenseCreateManySchema } from '../generated/schemas/createManyExpense.schema';
 import { ExpenseCreateOneSchema } from '../generated/schemas/createOneExpense.schema';
-
-const INCOMES_CATEGORIES = [
-  {
-    id: 1,
-    name: 'Parent',
-  },
-  {
-    id: 2,
-    name: 'Claim',
-  },
-  {
-    id: 3,
-    name: 'Salary',
-  },
-  {
-    id: 4,
-    name: 'Blessing',
-  },
-  {
-    id: 5,
-    name: 'Bill Split',
-  },
-];
 
 export const financeRouter = t.router({
   findManyExpense: t.procedure
@@ -37,5 +14,4 @@ export const financeRouter = t.router({
   createOneExpense: t.procedure
     .input(ExpenseCreateOneSchema)
     .mutation(({ ctx, input }) => ctx.prisma.expense.create(input)),
-  seed: t.procedure.mutation(({ ctx }) => {}),
 });
